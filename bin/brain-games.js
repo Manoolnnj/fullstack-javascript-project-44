db112620c24340207bbec0b6c51e48812436eab0
Vitalii Naida <nvs.fop@gmail.com>
2023-01-08 04:29:11 +0100

    3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-/*
+
 import { readlineSync } from 'readline-sync';
 import { name } from '../src/cli.js';
 import { evenGame } from '../games/brain-even.js';
-*/
+import { greetings } from '../src/cli.js';
+import { question } from '../src/cli.js';
 
+
+greetings();
+const name = question();
