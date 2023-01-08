10fb688a47112443c99d6dc38682e8b642fb1691
Vitalii Naida <nvs.fop@gmail.com>
2023-01-08 04:35:07 +0100

    4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import { readlineSync } from 'readline-sync';
-import { evenGame } from '../games/brain-even.js';
 import { greetings } from '../src/cli.js';
 import { question } from '../src/cli.js';
 
