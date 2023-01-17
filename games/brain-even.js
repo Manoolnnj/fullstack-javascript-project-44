25d3a202e62bc34e5785932802248868d78cbf59
Vitalii Naida <nvs.fop@gmail.com>
2023-01-17 11:53:58 +0100

    remaking

--- games/brain-even.js
+++ games/brain-even.js
@@ -17,27 +17,28 @@ const evenGame = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   for (let i = 0; i < 3; i += 1) {
-    const number = randomInteger(0, 50);
+    const number = randomInteger(1, 100);
     console.log(`Question: ${number}`);
     const userAnswer = readlineSync.question('Your answer ');
 
-    const check1 = (number % 2 === 0) && (userAnswer === 'yes');
-    const check2 = (number % 2 !== 0) && (userAnswer === 'no');
-
-    if (check1 === true || check2 === true) {
+    if ((isEven(number))) && ((userAnswer === 'yes')) {
       console.log('Correct!');
-    } else {
-      if (isEven(number)) {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, ${name}!`);
-        return;
-      } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'no'\nLet's try again, ${name}!`);
-        return;
-      }
     }
+    else if ((!(isEven(number))) && (userAnswer === 'no')) {
+  console.log('Correct!!');
+} else {
+  if (isEven(number)) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, ${name}!`);
+    return;
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'no'\nLet's try again, ${name}!`);
+    return;
   }
-  console.log(`Congratulations, ${name}!`);
-};
+}
+  }
+}
+console.log(`Congratulations, ${name}!`);
+
 
 evenGame();
 export default evenGame;
