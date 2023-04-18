732647006fab0d59472aa3eaefad5ff8438a512b
Manoolnnj <nvs.pers@gmail.com>
2023-04-18 09:41:07 +0200

    rewriting brain-pregression

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -1,16 +1,9 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import { randomInteger } from '../src/index.js';
-import { greetings, question } from '../src/cli.js';
+import { gameLogic, randomInteger } from '../src/index.js';
 
-greetings();
-const name = question();
 
-const progression = () => {
-  console.log('What number is missing in the progression ?');
-
-  for (let i = 0; i < 3; i += 1) {
+const makeRound = () => {
     const initialTerm = randomInteger(1, 20);
     const commonDifference = randomInteger(1, 7);
     const arr = [initialTerm];
@@ -23,17 +16,11 @@ const progression = () => {
     }
     const arrClone = arr.slice(0);
     arrClone[hiddenIndex] = '..';
-    console.log(`Question: ${arrClone.join(' ')}`);
-    const userAnswer = readlineSync.question('Your answer ');
-
-    if (userAnswer.toString() === arr[hiddenIndex].toString()) {
-      console.log('Correct!');
-    } else {
-      console.log(`${userAnswer} is wrong answer; (.Correct answer was '${arr[hiddenIndex]}'.\nLet's try again, ${name}!`);
-      return;
-    }
+    const question = arrClone.join(' ');
+    const correctAnswer = arr[hiddenIndex].toString()
+    return [question, correctAnswer];
   }
-  console.log(`Congratulations, ${name}!`);
-};
 
-progression();
+const task = 'What number is missing in the progression ?';
+
+gameLogic(task, makeRound);
