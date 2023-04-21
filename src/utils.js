6ac4a15e89d71cc99c10f3b8f7fa4469af57b34a
Manoolnnj <nvs.pers@gmail.com>
2023-04-21 18:26:31 +0200

    final fixes 9

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
-export default const randomInteger = (min, max) => {
+const randomInteger = (min, max) => {
   const random = min + Math.random() * (max + 1 - min);
   return Math.floor(random);
 };
+
+export default randomInteger;
